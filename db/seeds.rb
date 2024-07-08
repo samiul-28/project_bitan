ec2cb948d6d90a87d9f76ae3875d22a65abe1dd5
Samiul <s.a.samiru64@gmail.com>
2024-07-08 15:43:32 +0600

    simple crud and authenticattion

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,9 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+
+Product.create(name: "Samsung", variety: "China", ditails: "Android mobile phone", price: 12000.0, mfg_date: "12/02/2024", exp_date: "23/09/2030")
+Product.create(name: "Symphony", variety: "China", ditails: "Android mobile phone", price: 14000.0, mfg_date: "12/02/2024", exp_date: "23/09/2030")
+Product.create(name: "Sony", variety: "korea", ditails: "Android mobile phone", price: 18000.0, mfg_date: "12/02/2024", exp_date: "23/09/2030")
+Product.create(name: "Apple", variety: "usa", ditails: "Apple mobile phone", price: 80000.0, mfg_date: "12/02/2024", exp_date: "23/09/2030")
