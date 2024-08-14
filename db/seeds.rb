f2619eac9d94d03802d90c6451c3aa03a77ebd6b
Samiul <s.a.samiru64@gmail.com>
2024-08-14 14:53:59 +0600

    New data add

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,3 +13,8 @@ Product.create(name: "Samsung", variety: "China", ditails: "Android mobile phone
 Product.create(name: "Symphony", variety: "China", ditails: "Android mobile phone", price: 14000.0, mfg_date: "12/02/2024", exp_date: "23/09/2030")
 Product.create(name: "Sony", variety: "korea", ditails: "Android mobile phone", price: 18000.0, mfg_date: "12/02/2024", exp_date: "23/09/2030")
 Product.create(name: "Apple", variety: "usa", ditails: "Apple mobile phone", price: 80000.0, mfg_date: "12/02/2024", exp_date: "23/09/2030")
+
+Product.create(name: "Hero", variety: "indian", ditails: "Indian 100cc bike", price: 100000.0, mfg_date: "12/02/2024", exp_date: "23/09/2030")
+Product.create(name: "Honda", variety: "indian", ditails: "Indian 100cc bike", price: 120000.0, mfg_date: "1/02/2024", exp_date: "23/09/2080")
+Product.create(name: "Tvs", variety: "Japani", ditails: "japani 125cc bike", price: 20000.0, mfg_date: "12/02/2024", exp_date: "23/09/2030")
+Product.create(name: "Yamaha", variety: "Japani", ditails: " 175ccjapani bike", price: 50000.0, mfg_date: "12/02/2024", exp_date: "23/09/2030")
